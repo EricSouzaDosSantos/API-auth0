6d93d23e9758f4e277401bc598c89aa1c10112ef
Eric <ericsouza94895@gmail.com>
2024-10-15 14:40:58 -0300

    feat: initial commit

---
+++ src/main/java/com/senai/anchieta/PontinhaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.anchieta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontinhaApplication.class, args);
+	}
+
+}
